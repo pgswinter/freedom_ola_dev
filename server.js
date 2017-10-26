fe6f99d3950c55bd1d7e91cee27c8bccbe170b32
unknown <tuananh.nguyen.freelance@gmail.com>
2017-10-26 21:50:54 +0700

    Update OLA

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-import express from 'express'
+// import express from 'express'
+
+var express = require('express');
+
+var config = require('./webpack.dev.js');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
 
 const app = express();
+var compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler,{noInfo: true, publicPath:config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
 
 app.set('view engine', 'pug');
 
